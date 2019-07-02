66a61719c66977ae3d807bfcdd4b6f59d0c98b90
Alisson <alisson-henri@live.com>
2019-07-02 18:43:57 -0300

    cors tentativa

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const databaseConfig = require("./config/database");
-
+const corsOptions = {
+  origin: "https://fcefriendfront.herokuapp.com",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 class App {
   constructor() {
     this.express = express();
@@ -21,7 +24,7 @@ class App {
   }
   middlewares() {
     this.express.use(express.json());
-    this.express.use(cors({ origin: "*" }));
+    this.express.use(cors(corsOptions));
   }
 
   routes() {
