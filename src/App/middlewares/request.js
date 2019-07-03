a7a7630c84a99bb7ec0317f317e339d1c5b7160c
Alisson <alisson-henri@live.com>
2019-07-03 09:17:57 -0300

    cor tentativa 12

---
+++ src/App/middlewares/request.js
@@ -0,0 +1,13 @@
+module.exports = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, OPTIONS, PATCH, DELETE, HEAD"
+  );
+  next();
+};
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const SessionController = require("./App/controllers/SessionController");
 const AdFriendController = require("./App/controllers/AdFriendController");
 const authMiddleware = require("./App/middlewares/auth");
 const MaillController = require("./App/controllers/MaillController");
+const Request = require("./App/middlewares/request");
 
 routes.post("/users", UserController.store);
 routes.post("/session", SessionController.store);
@@ -14,7 +15,7 @@ routes.post("/mail", MaillController.store);
 routes.get("/test", (req, res) => res.send("ok"));
 
 //rotas autenticadas
-routes.use(authMiddleware);
+routes.use(authMiddleware, Request);
 routes.get("/users", UserController.index);
 ///rotas register
 routes.get("/ads", AdFriendController.index);
